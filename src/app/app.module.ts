8978ce83e47d64ad507f87ea9d15cdfcb5b4bb3b
KOMPIUTERIS\Benjaminas <gruodys@gmail.com>
2017-10-29 20:57:17 +0200

    no message

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderCarouselComponent } from './header-carousel/header-carousel.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [AppComponent, HeaderCarouselComponent, MenuComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ src/app/header-carousel/header-carousel.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'header-carousel',
+    templateUrl: './header-carousel.component.html',
+})
+export class HeaderCarouselComponent {  }
